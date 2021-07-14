bb28985d6eb575b28630d5b395602207aa246096
Alexey <sisipun@gmail.com>
2021-07-14 20:45:37 +0300

    Face culling

--- opengl/renderer.cpp
+++ opengl/renderer.cpp
@@ -5,7 +5,7 @@
 #include <glad/glad.h>
 #include <stbimage/stb_image.h>
 
-Renderer::Renderer(int screenWidth, int screeHeight) : screenWidth(screenWidth), screeHeight(screeHeight), shader(Shader("../shader.vert", "../shader.geom", "../shader.frag")), camera(Camera(glm::vec3(0.0, 3.0, 3.0f), glm::vec3(0.0f, 0.1f, 0.0f), float(screenWidth) / float(screeHeight), 100.0f, 0.0f, -90.0f, 0.0f, 90.0f)), light(DirLight(glm::vec3(0.2f), glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(3.0f, -3.0f, 300.0f)))
+Renderer::Renderer(int screenWidth, int screeHeight) : screenWidth(screenWidth), screeHeight(screeHeight), shader(Shader("../shader.vert", "../shader.geom", "../shader.frag")), camera(Camera(glm::vec3(0.0, 3.0, 3.0f), glm::vec3(0.0f, 0.1f, 0.0f), float(screenWidth) / float(screeHeight), 100.0f, 0.0f, -90.0f, 0.0f, 90.0f)), light(DirLight(glm::vec3(0.2f), glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(3.0f, -3.0f, 3.0f)))
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -33,6 +33,7 @@ Renderer::Renderer(int screenWidth, int screeHeight) : screenWidth(screenWidth),
 
     stbi_set_flip_vertically_on_load(true);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
 
     shader.load();
 }
