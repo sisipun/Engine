c52e7d8d1fa0b4d0819be8c4fb1d05b816b7b205
Sisipun <sisipun@gmail.com>
2021-07-18 14:11:40 +0300

    direct x

---
+++ directx/directx/main.cpp
@@ -0,0 +1,88 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, L"FDownWindow");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, L"FUpWindow");
+		}
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+)
+{
+	const auto className = L"DirectX";
+
+	WNDCLASSEX wc = {};
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = className;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindowEx(
+		0,
+		className,
+		L"Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200,
+		200,
+		640,
+		480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+
+	if (hWnd == nullptr)
+	{
+		return -1;
+	}
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	MSG msg = {};
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+}
