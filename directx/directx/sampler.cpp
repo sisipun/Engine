c5a4af649d4131256ff7b19c6ecc48db11943755
Sisipun <sisipun@gmail.com>
2021-12-03 20:05:56 +0300

    DirectX - add mip map

--- directx/directx/sampler.cpp
+++ directx/directx/sampler.cpp
@@ -6,10 +6,14 @@ Sampler::Sampler(const Renderer& renderer)
 	HRESULT hResult;
 
 	D3D11_SAMPLER_DESC description = {};
-	description.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	description.Filter = D3D11_FILTER_ANISOTROPIC;
+	description.MaxAnisotropy = D3D11_REQ_MAXANISOTROPY;
 	description.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	description.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	description.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	description.MipLODBias = 0.0f;
+	description.MinLOD = 0.0f;
+	description.MaxLOD = D3D11_FLOAT32_MAX;
 
 	RENDERER_THROW_NOINFO(hResult, renderer.getDevice()->CreateSamplerState(&description, &sampler));
 }
--- directx/directx/texture.cpp
+++ directx/directx/texture.cpp
@@ -11,29 +11,29 @@ Texture::Texture(const Renderer& renderer, const std::string& path, unsigned int
 	D3D11_TEXTURE2D_DESC textureDescription = {};
 	textureDescription.Width = surface.getWidth();
 	textureDescription.Height = surface.getHeight();
-	textureDescription.MipLevels = 1;
+	textureDescription.MipLevels = 0;
 	textureDescription.ArraySize = 1;
 	textureDescription.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 	textureDescription.SampleDesc.Count = 1;
 	textureDescription.SampleDesc.Quality = 0;
 	textureDescription.Usage = D3D11_USAGE_DEFAULT;
-	textureDescription.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	textureDescription.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
 	textureDescription.CPUAccessFlags = 0;
-	textureDescription.MiscFlags = 0;
-	D3D11_SUBRESOURCE_DATA data = {};
-	data.pSysMem = surface.getBufferPointer();
-	data.SysMemPitch = surface.getWidth() * sizeof(Surface::Color);
+	textureDescription.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
 	Microsoft::WRL::ComPtr<ID3D11Texture2D> texture;
 	
-	RENDERER_THROW_NOINFO(hResult, renderer.getDevice()->CreateTexture2D(&textureDescription, &data, &texture));
+	RENDERER_THROW_NOINFO(hResult, renderer.getDevice()->CreateTexture2D(&textureDescription, nullptr, &texture));
+
+	renderer.getContext()->UpdateSubresource(texture.Get(), 0, nullptr, surface.getBufferPointerConst(), surface.getWidth() * sizeof(Surface::Color), 0);
 
 	D3D11_SHADER_RESOURCE_VIEW_DESC shaderViewDescription = {};
 	shaderViewDescription.Format = textureDescription.Format;
 	shaderViewDescription.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 	shaderViewDescription.Texture2D.MostDetailedMip = 0;
-	shaderViewDescription.Texture2D.MipLevels = 1;
+	shaderViewDescription.Texture2D.MipLevels = -1;
 
 	RENDERER_THROW_NOINFO(hResult, renderer.getDevice()->CreateShaderResourceView(texture.Get(), &shaderViewDescription, &textureView));
+	renderer.getContext()->GenerateMips(textureView.Get());
 }
 
 void Texture::bind(const Renderer& renderer) noexcept
