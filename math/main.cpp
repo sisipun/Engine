46e1aedeabc2cc14b3e2034385dbe2413c315f81
Alexey <sisipun@gmail.com>
2021-12-27 20:04:10 +0300

    Math Engine - Matrix add and substract

--- math/main.cpp
+++ math/main.cpp
@@ -2,6 +2,7 @@
 
 #include "vector/vector2.h"
 #include "vector/vector3.h"
+#include "matrix/matrix.h"
 
 int main()
 {
@@ -14,6 +15,7 @@ int main()
     Vector<2> v2Div = v2Second / 2.0;
     Vector<2> v2Ref = v2Second.reflect(Vector<2>(1, 1));
     float v2Dot = v2First.dot(v2Second);
+    v2First.normalize();
 
     std::cout << "Vector2 - Sum: (" << v2Sum.x << "," << v2Sum.y << ")" << std::endl;
     std::cout << "Vector2 - Sub: (" << v2Sub.x << "," << v2Sub.y << ")" << std::endl;
@@ -22,8 +24,6 @@ int main()
     std::cout << "Vector2 - Ref: (" << v2Ref.x << "," << v2Ref.y << ")" << std::endl;
     std::cout << "Vector2 - Dot: (" << v2Dot << ")" << std::endl;
     std::cout << "Vector2 - Len: (" << v2First.length() << ")" << std::endl;
-
-    v2First.normalize();
     std::cout << "Vector2 - Norm: (" << v2First.x << "," << v2First.y << ")" << std::endl;
 
     Vector<3> v3First(4, -2, 5);
@@ -35,6 +35,7 @@ int main()
     Vector<3> v3Ref = v3Second.reflect(Vector<3>(1, 0, 0));
     float v3Dot = v3First.dot(v3Second);
     Vector<3> v3Cross = v3First.cross(v3Second);
+    v3First.normalize();
 
     std::cout << "Vector3 - Sum: (" << v3Sum.x << "," << v3Sum.y << "," << v3Sum.z << ")" << std::endl;
     std::cout << "Vector3 - Sub: (" << v3Sub.x << "," << v3Sub.y << "," << v3Sub.z << ")" << std::endl;
@@ -44,7 +45,13 @@ int main()
     std::cout << "Vector3 - Dot: (" << v3Dot << ")" << std::endl;
     std::cout << "Vector3 - Crs: (" << v3Cross.x << "," << v3Cross.y << "," << v3Cross.z << ")" << std::endl;
     std::cout << "Vector3 - Len: (" << v3First.length() << ")" << std::endl;
-
-    v3First.normalize();
     std::cout << "Vector3 - Norm: (" << v3First.x << "," << v3First.y << "," << v3First.z << ")" << std::endl;
+
+    Matrix<2, 2> matrixFirst({0.5f, 3.0f, 0.0f, -1.4f});
+    Matrix<2, 2> matrixSecond({1.0f, 2.0f, 3.0f, 4.0f});
+    Matrix<2, 2> matrixSum = matrixSecond + matrixFirst;
+    Matrix<2, 2> matrixSub = matrixSecond - matrixFirst;
+
+    std::cout << "Matrix - Sum: \n" << matrixSum.data[0] << ", " << matrixSum.data[1] << "\n" << matrixSum.data[2] << ", " << matrixSum.data[3] << "\n" << std::endl;
+    std::cout << "Matrix - Sub: \n" << matrixSub.data[0] << ", " << matrixSub.data[1] << "\n" << matrixSub.data[2] << ", " << matrixSub.data[3] << "\n" << std::endl;
 }
--- math/matrix/matrix.h
+++ math/matrix/matrix.h
@@ -5,8 +5,57 @@ namespace pickle
 {
     namespace math
     {
-        template<int R, int C>
-        class Matrix;
+        template <int R, int C>
+        class Matrix
+        {
+        public:
+            Matrix() : data{0.0f}
+            {
+            }
+
+            Matrix(const float (&data)[R * C])
+            {
+                for (int i = 0; i < R; i++)
+                {
+                    for (int j = 0; j < C; j++)
+                    {
+                        int index = i * C + j;
+                        this->data[index] = data[index];
+                    }
+                }
+            }
+
+            Matrix<R, C> operator+(const Matrix<R, C> &matrix)
+            {
+                Matrix<R, C> sum;
+                for (int i = 0; i < R; i++)
+                {
+                    for (int j = 0; j < C; j++)
+                    {
+                        int index = i * C + j;
+                        sum.data[index] = data[index] + matrix.data[index];
+                    }
+                }
+                return sum;
+            }
+
+            Matrix<R, C> operator-(const Matrix<R, C> &matrix)
+            {
+                Matrix<R, C> sub;
+                for (int i = 0; i < R; i++)
+                {
+                    for (int j = 0; j < C; j++)
+                    {
+                        int index = i * C + j;
+                        sub.data[index] = data[index] - matrix.data[index];
+                    }
+                }
+                return sub;
+            }
+
+        public:
+            float data[R * C];
+        };
     }
 }
 
