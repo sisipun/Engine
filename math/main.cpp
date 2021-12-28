1a9c248e3c6b2a56537b0351fa0b8df57c96efba
Alexey <sisipun@gmail.com>
2021-12-28 19:19:49 +0300

    Math Engine - matrix on scalar multiplication and division

--- math/main.cpp
+++ math/main.cpp
@@ -8,6 +8,7 @@ int main()
 {
     using namespace pickle::math;
     Vector<2> v2First(1, 1);
+    Vector<2> v2Cpy = v2First;
     Vector<2> v2Second(-1, 0);
     Vector<2> v2Sum = v2First + v2Second;
     Vector<2> v2Sub = v2First - v2Second;
@@ -17,6 +18,7 @@ int main()
     float v2Dot = v2First.dot(v2Second);
     v2First.normalize();
 
+    std::cout << "Vector2 - Cpy: (" << v2Cpy.x << "," << v2Cpy.y << ")" << std::endl;
     std::cout << "Vector2 - Sum: (" << v2Sum.x << "," << v2Sum.y << ")" << std::endl;
     std::cout << "Vector2 - Sub: (" << v2Sub.x << "," << v2Sub.y << ")" << std::endl;
     std::cout << "Vector2 - Mul: (" << v2Mul.x << "," << v2Mul.y << ")" << std::endl;
@@ -27,6 +29,7 @@ int main()
     std::cout << "Vector2 - Norm: (" << v2First.x << "," << v2First.y << ")" << std::endl;
 
     Vector<3> v3First(4, -2, 5);
+    Vector<3> v3Cpy = v3First;
     Vector<3> v3Second(-1, -1, -1);
     Vector<3> v3Sum = v3First + v3Second;
     Vector<3> v3Sub = v3First - v3Second;
@@ -37,6 +40,7 @@ int main()
     Vector<3> v3Cross = v3First.cross(v3Second);
     v3First.normalize();
 
+    std::cout << "Vector3 - Cpy: (" << v3Cpy.x << "," << v3Cpy.y << "," << v3Cpy.z << ")" << std::endl;
     std::cout << "Vector3 - Sum: (" << v3Sum.x << "," << v3Sum.y << "," << v3Sum.z << ")" << std::endl;
     std::cout << "Vector3 - Sub: (" << v3Sub.x << "," << v3Sub.y << "," << v3Sub.z << ")" << std::endl;
     std::cout << "Vector3 - Mul: (" << v3Mul.x << "," << v3Mul.y << "," << v3Mul.z << ")" << std::endl;
@@ -48,10 +52,16 @@ int main()
     std::cout << "Vector3 - Norm: (" << v3First.x << "," << v3First.y << "," << v3First.z << ")" << std::endl;
 
     Matrix<2, 2> matrixFirst({0.5f, 3.0f, 0.0f, -1.4f});
+    Matrix<2, 2> matrixCpy = matrixFirst;
     Matrix<2, 2> matrixSecond({1.0f, 2.0f, 3.0f, 4.0f});
     Matrix<2, 2> matrixSum = matrixSecond + matrixFirst;
     Matrix<2, 2> matrixSub = matrixSecond - matrixFirst;
+    Matrix<2, 2> matrixMul = matrixFirst * 2;
+    Matrix<2, 2> matrixDiv = matrixSecond / 2;
 
+    std::cout << "Matrix - Cpy: \n" << matrixCpy.data[0] << ", " << matrixCpy.data[1] << "\n" << matrixCpy.data[2] << ", " << matrixCpy.data[3] << "\n" << std::endl;
     std::cout << "Matrix - Sum: \n" << matrixSum.data[0] << ", " << matrixSum.data[1] << "\n" << matrixSum.data[2] << ", " << matrixSum.data[3] << "\n" << std::endl;
     std::cout << "Matrix - Sub: \n" << matrixSub.data[0] << ", " << matrixSub.data[1] << "\n" << matrixSub.data[2] << ", " << matrixSub.data[3] << "\n" << std::endl;
+    std::cout << "Matrix - Mul: \n" << matrixMul.data[0] << ", " << matrixMul.data[1] << "\n" << matrixMul.data[2] << ", " << matrixMul.data[3] << "\n" << std::endl;
+    std::cout << "Matrix - Div: \n" << matrixDiv.data[0] << ", " << matrixDiv.data[1] << "\n" << matrixDiv.data[2] << ", " << matrixDiv.data[3] << "\n" << std::endl;
 }
--- math/matrix/matrix.h
+++ math/matrix/matrix.h
@@ -25,6 +25,30 @@ namespace pickle
                 }
             }
 
+            Matrix(const Matrix &matrix) : Matrix(matrix.data)
+            {
+            }
+
+            ~Matrix() = default;
+
+            Matrix<R, C> &operator=(const Matrix<R, C> &matrix)
+            {
+                if (this == &matrix)
+                {
+                    return *this;
+                }
+
+                for (int i = 0; i < R; i++)
+                {
+                    for (int j = 0; j < C; j++)
+                    {
+                        int index = i * C + j;
+                        this->data[index] = matrix.data[index];
+                    }
+                }
+                return *this;
+            }
+
             Matrix<R, C> operator+(const Matrix<R, C> &matrix)
             {
                 Matrix<R, C> sum;
@@ -51,6 +75,34 @@ namespace pickle
                     }
                 }
                 return sub;
+            }
+
+            Matrix<R, C> operator*(float scalar) const
+            {
+                Matrix<R, C> mul;
+                for (int i = 0; i < R; i++)
+                {
+                    for (int j = 0; j < C; j++)
+                    {
+                        int index = i * C + j;
+                        mul.data[index] = data[index] * scalar;
+                    }
+                }
+                return mul;
+            }
+
+            Matrix<R, C> operator/(float divider) const
+            {
+                Matrix<R, C> div;
+                for (int i = 0; i < R; i++)
+                {
+                    for (int j = 0; j < C; j++)
+                    {
+                        int index = i * C + j;
+                        div.data[index] = data[index] / divider;
+                    }
+                }
+                return div;
             }
 
         public:
