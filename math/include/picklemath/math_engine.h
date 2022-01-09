48c936275c844b2dc82a660df9f6df9712043920
Alexey <sisipun@gmail.com>
2022-01-09 14:46:59 +0300

    Renderer - create renderer module

--- math/include/picklemath/math_engine.h
+++ math/include/picklemath/math_engine.h
@@ -1,5 +1,10 @@
+#ifndef PICKLE_MATH
+#define PICKLE_MATH
+
 #include "vector.h"
 #include "vector_operations.h"
 #include "matrix.h"
 #include "matrix_operations.h"
 #include "transform_operations.h"
+
+#endif
---
+++ renderer/include/picklerenderer/renderer.h
@@ -0,0 +1,15 @@
+#ifndef PICKLE_RENDERER
+#define PICKLE_RENDERER
+
+namespace pickle
+{
+    namespace renderer
+    {
+        class Renderer
+        {
+            virtual void render() = 0;
+        };
+    }
+}
+
+#endif
