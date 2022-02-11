ab8ff9004a1251d39c9345e48de3f287d63518a7
Alexey Kadach <sisipun@gmail.com>
2022-02-11 16:59:56 +0200

    Procedural - raymarching sphere

--- procedural/main.cpp
+++ procedural/main.cpp
@@ -1,160 +1,149 @@
 #include <SDL.h>
-
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include <glad/glad.h>
 
-void checkShaderCompilation(unsigned int shader)
-{
-    int success;
-    char log[1024];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(shader, 1024, NULL, log);
-        std::cout << "Can't compile shader: " << log << std::endl;
-    }
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+void checkShaderCompilation(unsigned int shader) {
+  int success;
+  char log[1024];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(shader, 1024, NULL, log);
+    std::cout << "Can't compile shader: " << log << std::endl;
+  }
 }
 
-void checkProgramLinking(unsigned int program)
-{
-    int success;
-    char log[1024];
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(program, 1024, NULL, log);
-        std::cout << "Can't link programm: " << log << std::endl;
-    }
+void checkProgramLinking(unsigned int program) {
+  int success;
+  char log[1024];
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(program, 1024, NULL, log);
+    std::cout << "Can't link programm: " << log << std::endl;
+  }
 }
 
-int main(int argc, char* argv[])
-{
-    float width = 800;
-    float height = 600;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "Can't load SDL module" << std::endl;
-    }
+int main(int argc, char* argv[]) {
+  float width = 800;
+  float height = 600;
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "Can't load SDL module" << std::endl;
+  }
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    SDL_Window* window = SDL_CreateWindow("Procedural", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
-    if (window == nullptr)
-    {
-        std::cout << "Can't create window" << std::endl;
-    }
+  SDL_Window* window = SDL_CreateWindow("Procedural", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, width, height,
+                                        SDL_WINDOW_OPENGL);
+  if (window == nullptr) {
+    std::cout << "Can't create window" << std::endl;
+  }
 
-    SDL_GLContext context = SDL_GL_CreateContext(window);
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
-    {
-        std::cout << "Can't load glad module" << std::endl;
-    }
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+  if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    std::cout << "Can't load glad module" << std::endl;
+  }
 
-    std::ifstream vertexShaderFile("../shader.vert");
-    std::ifstream fragmentShaderFile("../shader.frag");
+  std::ifstream vertexShaderFile("../shader.vert");
+  std::ifstream fragmentShaderFile("../shader.frag");
 
-    if (!vertexShaderFile)
-    {
-        std::cout << "Can't read vertex file" << std::endl;
-    }
-    if (!fragmentShaderFile)
-    {
-        std::cout << "Can't read fragment file" << std::endl;
-    }
+  if (!vertexShaderFile) {
+    std::cout << "Can't read vertex file" << std::endl;
+  }
+  if (!fragmentShaderFile) {
+    std::cout << "Can't read fragment file" << std::endl;
+  }
 
-    std::stringstream vertexShaderContentStream;
-    std::stringstream fragmentShaderContentStream;
+  std::stringstream vertexShaderContentStream;
+  std::stringstream fragmentShaderContentStream;
 
-    vertexShaderContentStream << vertexShaderFile.rdbuf();
-    fragmentShaderContentStream << fragmentShaderFile.rdbuf();
+  vertexShaderContentStream << vertexShaderFile.rdbuf();
+  fragmentShaderContentStream << fragmentShaderFile.rdbuf();
 
-    vertexShaderFile.close();
-    fragmentShaderFile.close();
+  vertexShaderFile.close();
+  fragmentShaderFile.close();
 
-    std::string vertexShaderContentString = vertexShaderContentStream.str();
-    std::string fragmentShaderContentString = fragmentShaderContentStream.str();
+  std::string vertexShaderContentString = vertexShaderContentStream.str();
+  std::string fragmentShaderContentString = fragmentShaderContentStream.str();
 
-    const char* vertexShaderContent = vertexShaderContentString.c_str();
-    const char* fragmentShaderContent = fragmentShaderContentString.c_str();
+  const char* vertexShaderContent = vertexShaderContentString.c_str();
+  const char* fragmentShaderContent = fragmentShaderContentString.c_str();
 
-    int vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-    int fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+  int vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+  int fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
 
-    glShaderSource(vertexShaderId, 1, &vertexShaderContent, NULL);
-    glShaderSource(fragmentShaderId, 1, &fragmentShaderContent, NULL);
+  glShaderSource(vertexShaderId, 1, &vertexShaderContent, NULL);
+  glShaderSource(fragmentShaderId, 1, &fragmentShaderContent, NULL);
 
-    glCompileShader(vertexShaderId);
-    glCompileShader(fragmentShaderId);
+  glCompileShader(vertexShaderId);
+  glCompileShader(fragmentShaderId);
 
-    checkShaderCompilation(vertexShaderId);
-    checkShaderCompilation(fragmentShaderId);
+  checkShaderCompilation(vertexShaderId);
+  checkShaderCompilation(fragmentShaderId);
 
-    int shaderProgramId = glCreateProgram();
-    glAttachShader(shaderProgramId, vertexShaderId);
-    glAttachShader(shaderProgramId, fragmentShaderId);
-    glLinkProgram(shaderProgramId);
-    glDeleteShader(vertexShaderId);
-    glDeleteShader(fragmentShaderId);
+  int shaderProgramId = glCreateProgram();
+  glAttachShader(shaderProgramId, vertexShaderId);
+  glAttachShader(shaderProgramId, fragmentShaderId);
+  glLinkProgram(shaderProgramId);
+  glDeleteShader(vertexShaderId);
+  glDeleteShader(fragmentShaderId);
 
-    checkProgramLinking(shaderProgramId);
+  checkProgramLinking(shaderProgramId);
 
-    float vertices[] = { -1.0f, -1.0f, 0.0f,
-                        -1.0f, 1.0f, 0.0f,
-                        1.0f, -1.0f, 0.0f,
+  float vertices[] = {
+      -1.0f, -1.0f, 0.0f, -1.0f, 1.0f,  0.0f, 1.0f, -1.0f, 0.0f,
 
-                        -1.0f, 1.0f, 0.0f,
-                        1.0f, -1.0f, 0.0f,
-                        1.0f, 1.0f, 0.0f };
+      -1.0f, 1.0f,  0.0f, 1.0f,  -1.0f, 0.0f, 1.0f, 1.0f,  0.0f};
 
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+
+  glBindVertexArray(VAO);
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
+
+  bool quit = false;
+  SDL_Event event;
+  while (!quit) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgramId);
+
+    int resolutionUniformId =
+        glGetUniformLocation(shaderProgramId, "u_resolution");
+    glUniform2f(resolutionUniformId, width, height);
+    int timeUniformId = glGetUniformLocation(shaderProgramId, "u_time");
+    glUniform1f(timeUniformId, SDL_GetTicks() / 500.0f);
 
     glBindVertexArray(VAO);
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
+    glDrawArrays(GL_TRIANGLES, 0, 6);
     glBindVertexArray(0);
 
-    bool quit = false;
-    SDL_Event event;
-    while (!quit)
-    {
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgramId);
-
-        int resolutionUniformId = glGetUniformLocation(shaderProgramId, "u_resolution");
-        glUniform2f(resolutionUniformId, width, height);
-
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        glBindVertexArray(0);
-
-        if (SDL_PollEvent(&event) != 0)
-        {
-            if (event.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-        }
-        SDL_GL_SwapWindow(window);
+    if (SDL_PollEvent(&event) != 0) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
     }
+    SDL_GL_SwapWindow(window);
+  }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgramId);
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgramId);
 
-    SDL_GL_DeleteContext(context);
-    SDL_Quit();
+  SDL_GL_DeleteContext(context);
+  SDL_Quit();
 }
--- procedural/shader.frag
+++ procedural/shader.frag
@@ -1,11 +1,63 @@
 #version 330 core
 
+#define MAX_STEPS 100
+#define MAX_DISTANCE 100
+#define SURFACE_MIN_DIST 0.01
+
 layout(location = 0) out vec4 fragColor;
 
 uniform vec2 u_resolution;
+uniform float u_time;
+
+float GetDistance(vec3 position)
+{
+    vec4 sphere = vec4(0, 1, 6, 1);
+    float sphereDist = length(position - sphere.xyz) - sphere.w;
+    float planeDist = position.y;
+    return min(sphereDist, planeDist);
+}
+
+float RayMarching(vec3 cameraPosition, vec3 cameraDirection)
+{
+    float resDist = 0;
+    for (int i = 0; i < MAX_STEPS; i++)
+    {
+        float dist = GetDistance(cameraPosition + resDist * cameraDirection);       
+        resDist += dist;
+        if (dist < SURFACE_MIN_DIST || resDist > MAX_DISTANCE) break;
+    }
+    return resDist;
+}
+
+vec3 GetNormal(vec3 position)
+{
+    float e = 0.01;
+    float dx = GetDistance(vec3(position.x, position.y, position.z)) - GetDistance(vec3(position.x - e, position.y, position.z));
+    float dy = GetDistance(vec3(position.x, position.y, position.z)) - GetDistance(vec3(position.x, position.y - e, position.z));
+    float dz = GetDistance(vec3(position.x, position.y, position.z)) - GetDistance(vec3(position.x, position.y, position.z - e));
+    return normalize(vec3(dx, dy, dz));
+}
+
+float GetLight(vec3 position) {
+    vec3 lightPosition = vec3(0.0f, 5.0f, 6.0f);
+    lightPosition.xy += vec2(sin(u_time), cos(u_time)); 
+    vec3 lightDirection = normalize(lightPosition - position);
+    vec3 norm = GetNormal(position);
+    float dotValue = clamp(dot(lightDirection, norm), 0.0f, 1.0f);
+    
+    float lightToPositionDistance = length(lightPosition - position);
+    float positionToLightDistance = RayMarching(position + norm * SURFACE_MIN_DIST, lightDirection);
+    if (positionToLightDistance < lightToPositionDistance) dotValue *= 0.1f;
+
+    return dotValue;
+}
 
 void main()
 {
-    vec2 uv = (2.0 * gl_FragCoord.xy - u_resolution.xy) / u_resolution.xy;
-    fragColor = vec4(uv.x, uv.y, 0.0f, 1.0f);
+    vec2 uv = (gl_FragCoord.xy - 0.5f * u_resolution.xy) / u_resolution.y;
+    vec3 cameraPosition = vec3(0.0f, 1.0f, 0.0f);
+    vec3 cameraDirection = normalize(vec3(uv.x, uv.y, 1.0f));
+    float dist = RayMarching(cameraPosition, cameraDirection);
+    vec3 color = vec3(GetLight(cameraPosition + dist * cameraDirection));
+    fragColor = vec4(color, 1.0f);
 }
