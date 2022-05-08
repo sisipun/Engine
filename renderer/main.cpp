b50cc7d34fcd7352f360ecc0781b108aa723602c
Alexey Kadach <sisipun@gmail.com>
2022-05-08 20:48:05 +0100

    Add draw line method

--- renderer/main.cpp
+++ renderer/main.cpp
@@ -4,10 +4,27 @@
 #include <iostream>
 #include <sstream>
 
+#define SCREEN_WIDTH 800.0f
+#define SCREEN_HEIGHT 600.0f
+
+void drawLine(SDL_Renderer *renderer, float x1, float y1, float x2, float y2)
+{
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    for (float i = 0.0f; i < 1.0f; i += 0.01)
+    {
+        float x = i * x1 + (1 - i) * x2;
+        float y = i * y1 + (1 - i) * y2;
+        SDL_Rect rect = {
+            static_cast<int>(x * SCREEN_WIDTH),
+            static_cast<int>(y * SCREEN_HEIGHT),
+            static_cast<int>(1.0f),
+            static_cast<int>(1.0f)};
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    float width = 800;
-    float height = 600;
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         std::cout << "Can't load SDL module" << std::endl;
@@ -17,7 +34,7 @@ int main(int argc, char *argv[])
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    SDL_Window *window = SDL_CreateWindow("Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr)
     {
         std::cout << "Can't create window" << std::endl;
@@ -37,6 +54,9 @@ int main(int argc, char *argv[])
     {
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
+
+        drawLine(renderer, 0.2, 0.2, 0.8, 0.8);
+
         SDL_RenderPresent(renderer);
 
         if (SDL_PollEvent(&event) != 0)
