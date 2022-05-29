fc18636cfcb0b924ad0a0d2a24c3d19f8018a71e
Alexey Kadach <sisipun@gmail.com>
2022-05-29 22:42:38 +0100

    Rasterization - add draw line method

--- sandbox/rasterization/main.cpp
+++ sandbox/rasterization/main.cpp
@@ -1,6 +1,42 @@
 #include <SDL.h>
 #include <pickle/math.h>
 
+void drawPoint(SDL_Renderer *renderer, int x, int y)
+{
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_Rect rect = {x, y, 1, 1};
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+void drawLine(SDL_Renderer *renderer, pickle::math::Vector<2, int> start, pickle::math::Vector<2, int> end)
+{
+    bool swapped = false;
+    if (std::abs(end.data[1] - start.data[1]) > std::abs(end.data[0] - start.data[0]))
+    {
+        std::swap(start.data[0], start.data[1]);
+        std::swap(end.data[0], end.data[1]);
+        swapped = true;
+    }
+    if (start.data[0] > end.data[0])
+    {
+        std::swap(start, end);
+    }
+    float a = float(end.data[1] - start.data[1]) / float(end.data[0] - start.data[0]);
+    float b = start.data[0] - a * start.data[1];
+    for (int x = start.data[0]; x < end.data[0]; x++)
+    {
+        int y = a * x + b;
+        if (swapped)
+        {
+            drawPoint(renderer, y, x);
+        }
+        else
+        {
+            drawPoint(renderer, x, y);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     const int width = 600;
@@ -33,6 +69,10 @@ int main(int argc, char *argv[])
     SDL_RenderClear(sdlRenderer);
 
     // Render
+    drawLine(sdlRenderer, pickle::math::Vector<2, int>({100, 100}), pickle::math::Vector<2, int>({500, 500}));
+    drawLine(sdlRenderer, pickle::math::Vector<2, int>({400, 300}), pickle::math::Vector<2, int>({200, 200}));
+    drawLine(sdlRenderer, pickle::math::Vector<2, int>({100, 500}), pickle::math::Vector<2, int>({500, 500}));
+    drawLine(sdlRenderer, pickle::math::Vector<2, int>({100, 100}), pickle::math::Vector<2, int>({100, 500}));
 
     SDL_RenderPresent(sdlRenderer);
 
