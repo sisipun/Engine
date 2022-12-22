b7ec33aeb9437bc31857373667f214f439b59182
Alexey Kadach <sisipun@gmail.com>
2022-12-22 09:52:29 +0100

    Metal renderer init

---
+++ renderer/include/pickle/metal_renderer.h
@@ -0,0 +1,22 @@
+#ifndef PICKLE_RENDERER_METAL
+#define PICKLE_RENDERER_METAL
+
+#include "renderer.h"
+
+namespace pickle
+{
+    namespace renderer
+    {
+        class MetalRenderer : public Renderer
+        {
+        public:
+            MetalRenderer();
+
+            ~MetalRenderer() override;
+
+            void render() const override;
+        };
+    }
+}
+
+#endif
---
+++ renderer/src/metal_renderer.cpp
@@ -0,0 +1,17 @@
+#include <pickle/directx_renderer.h>
+
+#include <pickle/logger.h>
+
+pickle::renderer::MetalRenderer::MetalRenderer()
+{
+    LOG_INFO("Inited Metal");
+}
+
+pickle::renderer::MetalRenderer::~MetalRenderer()
+{
+}
+
+void pickle::renderer::MetalRenderer::render() const
+{
+    LOG_INFO("Rendered Metal");
+}
