e8cc6ed40a41d4db93300ae2dfe030301f9e3db3
Sisipun <sisipun@gmail.com>
2023-01-29 20:08:07 +0100

    Core - Show win32 window

--- core/src/win32_window.cpp
+++ core/src/win32_window.cpp
@@ -1,11 +1,15 @@
 #include <pickle/win32_window.h>
 
+#include <windows.h>
+#include <tchar.h>
+
 #include <pickle/metal_renderer.h>
 #include <pickle/directx_renderer.h>
 #include <pickle/logger.h>
 
 pickle::Win32Window::Win32Window(int width, int height) : Window(width, height)
 {
+    MessageBox(NULL, _T("Text"), _T("Caption"), MB_OK);
     renderer = std::make_unique<pickle::renderer::DirectXRenderer>();
 }
 
