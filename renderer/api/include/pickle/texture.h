8200dd4b33a4bb9a63be1d2cf1c72a46207686f4
sisipun <sisipun@gmail.com>
2024-01-03 13:13:07 +0100

    Renderer - add texture class

---
+++ renderer/api/include/pickle/texture.h
@@ -0,0 +1,14 @@
+#ifndef PICKLE_RENDERER_TEXTURE
+#define PICKLE_RENDERER_TEXTURE
+
+namespace pickle
+{
+    namespace renderer
+    {
+        class Texture
+        {
+        };
+    }
+}
+
+#endif
